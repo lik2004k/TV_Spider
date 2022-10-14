0c91f365fbfe039344f40a42c70e6ae04af3f8a2
lik2004k <81137247+lik2004k@users.noreply.github.com>
2022-10-14 09:45:57 +0800

    Update T4update.sh

--- T4update.sh
+++ T4update.sh
@@ -4,7 +4,7 @@ md5o="aaaa"
 dkdir=/opt/TV-Spider #(改成自己的目录)
 imgtag=tvsp:latest  #(改成自己的镜像TAG)
 contname=TV-Spider #(改成自己的容器名称)
-cmd='CMD ["gunicorn", "-w 4", "-b 0.0.0.0:8080", "app:app"]' #(从这里更改启动端口)
+cmd='CMD ["gunicorn", "-w 4", "-b 0.0.0.0:8010", "app:app"]' #(从这里更改启动端口)
 #判断目录是否存在
 if [ ! -d $dkdir ];then
  mkdir $dkdir
